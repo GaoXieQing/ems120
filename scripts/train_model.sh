da4e6d833f2fa4218c3f4e8c3ceb757295d214ea
PeterQ <74954016+GaoXieQing@users.noreply.github.com>
2025-05-22 15:14:26 +0800

    Add files via upload

---
+++ scripts/train_model.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+module load python/anaconda3/5.2.0
+export PATH=/work/sph-gaoxq/.conda/envs/pytorch-tt/bin:$PATH #需要修改自己在HPC中的pytorch环境路径及名称
+export CONDA_PREFIX=/work/sph-gaoxq/.conda/envs/pytorch-tt #需要修改自己在HPC中的pytorch环境路径及名称
+python /work/sph-gaoxq/projects/emergency_data_QC/train_model.py #修改为自己的文件路径
+
+
